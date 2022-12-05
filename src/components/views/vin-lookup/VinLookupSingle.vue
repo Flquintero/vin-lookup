515f5870719e300f81b3d8ccd5fa7750d174f90a
Francisco Quintero <flquintero1@gmail.com>
2022-12-05 12:49:41 -0500

    Added input field

--- src/components/views/vin-lookup/VinLookupSingle.vue
+++ src/components/views/vin-lookup/VinLookupSingle.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Single Vin</div>
+  <div>
+    <p>
+      Enter a VIN and we'll use our data partner to searchf for your vehicle
+    </p>
+    <v-text-field
+      clearable
+      label="Enter VIN"
+      variant="underlined"
+      class="v-col-xs-12 v-col-sm-6 my-1"
+    ></v-text-field>
+  </div>
 </template>
-
-<style scoped></style>
